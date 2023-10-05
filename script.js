c0cf9154bd3712da58ebdb53df20285801f879d0
PeterHellmuth <10523853+PeterHellmuth@users.noreply.github.com>
2023-10-04 19:38:24 -0500

    Add linkList methods to class

--- script.js
+++ script.js
@@ -6,18 +6,16 @@ class linkedList {
 
   append(value) {
     if (this.root === null) {
-      this.root = new node();
+      this.root = new node(value);
       this.tail = this.root;
     } else {
-      this.tail.nextNode = new node();
+      this.tail.nextNode = new node(value);
       this.tail = this.tail.nextNode;
     }
   }
 
   prepend(value) {
-    let newNode = new node();
-    newNode.nextNode = this.root;
-    this.root = newNode;
+    this.root = new node(value, this.root);
   }
 
   size() {
@@ -33,12 +31,110 @@ class linkedList {
       return 0;
     }
   }
+
+  head() {
+    return this.root;
+  }
+
+  rail() {
+    return this.tail;
+  }
+
+  at(index) {
+    if (this.root) {
+      let currentIndex = 0;
+      let currentNode = this.root;
+      while (currentNode.nextNode != null) {
+        if (currentIndex == index) {
+          return currentNode;
+        }
+        currentIndex++;
+        currentNode = currentNode.nextNode;
+      }
+      if (currentIndex == index) {
+        return currentNode;
+      } else {
+        return null;
+      }
+    } else {
+      return null;
+    }
+  }
+  pop() {
+    if (this.root) {
+      let currentNode = this.root;
+      let lastNode = this.root;
+      while (currentNode.nextNode != null) {
+        lastNode = currentNode;
+        currentNode = currentNode.nextNode;
+      }
+      if (currentNode.nextNode == null) {
+        lastNode.nextNode = null;
+        return currentNode;
+      }
+    } else {
+      return null;
+    }
+  }
+  contains(value) {
+    if (this.root) {
+      let currentNode = this.root;
+      while (currentNode.nextNode != null) {
+        if (currentNode.value == value) {
+          return true;
+        }
+        currentNode = currentNode.nextNode;
+      }
+      if (currentNode.value == value) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  }
+  find(value) {
+    if (this.root) {
+      let currentIndex = 0;
+      let currentNode = this.root;
+      while (currentNode.nextNode != null) {
+        if (currentNode.value == value) {
+          return currentIndex;
+        }
+        currentIndex++;
+        currentNode = currentNode.nextNode;
+      }
+      if (currentNode.value == value) {
+        return currentIndex;
+      } else {
+        return null;
+      }
+    } else {
+      return null;
+    }
+  }
+
+  toString() {
+    if (this.root) {
+      let currentNode = this.root;
+      let returnString = `(${currentNode.value}) -> `;
+      while (currentNode.nextNode != null) {
+        currentNode = currentNode.nextNode;
+        returnString += `(${currentNode.value}) -> `;
+      }
+      returnString += `null`;
+      return returnString;
+    } else {
+      return "null";
+    }
+  }
 }
 
 class node {
-  constructor() {
-    this.value = null;
-    this.nextNode = null;
+  constructor(value = null, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
   }
 }
 
@@ -47,4 +143,7 @@ let aLinkedList = new linkedList();
 aLinkedList.append(5);
 aLinkedList.append(3);
 aLinkedList.append(2);
-console.log(aLinkedList.size());
+aLinkedList.append(7);
+aLinkedList.prepend(9);
+console.log(aLinkedList.toString());
+//console.log(aLinkedList.at(0));
